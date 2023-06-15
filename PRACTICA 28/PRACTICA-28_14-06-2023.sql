eb407aa3be6e416f5a92ddb422c2ac4af58a858d
xX-Higgs-Xx <20223tn131@utez.edu.mx>
2023-06-14 23:18:12 -0600

    Actualizacion PRACTICA 28 14/06/2023

--- PRACTICA 28/PRACTICA-28_14-06-2023.sql
+++ PRACTICA 28/PRACTICA-28_14-06-2023.sql
@@ -1,6 +1,9 @@
 create database practica;
 use practica;
 
+/*Paso 1:
+Crear las tablas y poblarlas con datos Crearemos las tablas 'clientes' y 'pedidos' con
+los siguientes campos y relaciones:*/
 create table clientes (
 id_cliente integer auto_increment,
 nombre varchar(255) not null,
@@ -25,9 +28,6 @@ INSERT INTO CLIENTES VALUES (4,'Arturo','arturo@gmail.com',8888888);
 
 INSERT INTO PEDIDOS VALUES (1,1,("2023-06-15"),1200);	
 INSERT INTO PEDIDOS VALUES (2,1,("2023-07-25"),2400);
-INSERT INTO PEDIDOS VALUES (3,1, ("2023-08-18"),300);
-INSERT INTO PEDIDOS VALUES (4,1, ("2023-09-15"),450);
-INSERT INTO PEDIDOS VALUES (5,1, ("2023-10-08"),200);
 #select * from clientes;
 
 /*Paso 2:
@@ -46,8 +46,8 @@ DELIMITER $$
 END;$$
 
 /*PASO 3:
-Crear un disparador 'BEFORE DELETE' para la tabla 'clientes' Crearemos un
-disparador que verifique si un cliente tiene pedidos asociados antes de eliminarlo.*/
+Probar el disparador 'BEFORE DELETE' Intente eliminar un cliente que tenga
+pedidos asociados y verifique si el disparador funciona correctamente:*/
 DELETE FROM clientes WHERE id_cliente=1;
 
 /*Paso 4:
@@ -74,9 +74,8 @@ BEGIN
 END;$$
 
 select * from clientes_eliminados;
-DROP TRIGGER registrar_eliminados;
 
-/*Paso 5: Probar el disparador 'AFTER UPDATE'
-Actualice el límite de crédito de un cliente y verifique si la columna
-'ultima_actualizacion' se actualiza correctamente.*/
-UPDATE clientes SET credito = 1000 where id_cliente = 1;
+/*Paso 5: 
+Probar el disparador 'AFTER DELETE' Elimine un cliente que no tenga pedidos
+asociados y verifique si se registra en la tabla 'clientes_eliminados'.*/
+DELETE FROM clientes WHERE id_cliente=4;
